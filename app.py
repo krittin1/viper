e7ec82585a404ebc23819cb3de52c97b6552c86a
Khan <k_h_an@hotmail.com>
2020-04-18 05:17:37 +0700

    My commitr

--- app.py
+++ app.py
@@ -20,20 +20,20 @@ from werkzeug.utils import secure_filename
 # graph = tf.get_default_graph()
 app = Flask(__name__)
 MODEL_PATH = 'models/cobra2.h5'
-GreenPit_PATH = 'models/GreenPitViper.h5'
-KingCobra_PATH = 'models/kingcobra.h5'
-russelViper_PATH = 'models/russelviper2.h5'
-malayanviper_PATH = 'models/malayanviper.h5'
-malayanKrait_PATH = 'models/malayanviper.h5'
-bandedKrait_PATH = 'models/malayanviper.h5'
+# GreenPit_PATH = 'models/GreenPitViper.h5'
+# KingCobra_PATH = 'models/kingcobra.h5'
+# russelViper_PATH = 'models/russelviper2.h5'
+# malayanviper_PATH = 'models/malayanviper.h5'
+# malayanKrait_PATH = 'models/malayanviper.h5'
+# bandedKrait_PATH = 'models/malayanviper.h5'
 
 model = load_model(MODEL_PATH)
-greenPit = load_model(GreenPit_PATH)
-KingCobra = load_model(KingCobra_PATH)
-RusselViper = load_model(russelViper_PATH)
-MalayanViper = load_model(malayanviper_PATH)
-MalayanKrait = load_model(malayanKrait_PATH)
-BandedKrait = load_model(bandedKrait_PATH)
+# greenPit = load_model(GreenPit_PATH)
+# KingCobra = load_model(KingCobra_PATH)
+# RusselViper = load_model(russelViper_PATH)
+# MalayanViper = load_model(malayanviper_PATH)
+# MalayanKrait = load_model(malayanKrait_PATH)
+# BandedKrait = load_model(bandedKrait_PATH)
 
 # BandedKrait._make_predict_function()
 # MalayanKrait._make_predict_function()
@@ -95,69 +95,63 @@ def upload():
 
 
 
-        snakeGroup = [model, greenPit, KingCobra, RusselViper,MalayanViper,MalayanKrait,BandedKrait]
+        snakeGroup = [model]
         data = {"TH": [
             {
                 "name": "งูเห่า",
                 "accuracy": 1.0,
-                "symptoms": "แสบบริเวณบาดแผล ปวดเล็กน้อย แขนขาจะไม่มีแรงและให้ความรู้สึกง่วงจนลืมตาไม่ขึ้น",
-                "aid": "ควรรีบมาโรงพยาบาลโดยเร็วที่สุด และขยับส่วนที่ถูกกัดให้น้อยเฉพาะเท่าที่จำเป็น เพื่อลดการดูดซึมพิษงูที่สำคัญ ถ้าถูกงูเห่าพ่นพิษเข้าตา ควรล้างตาด้วยน้ำสะอาดปริมาณมาก",
+                "symptoms": "แไม่ขึ้น",
+                "aid": "ควรา ควรล้างตาด้วยน้ำสะอาดปริมาณมาก",
                 "type": "มีพิษ",
                 "colorStyle": "red"
 
             },
-            {
-                "name": "งูเขียวหางไหม้",
-                "accuracy": 2.0,
-                "symptoms": "ผิวหนังพองเป็นถุงน้ำ มีเลือดซึมออกจากแผลรอยเขี้ยวพบรอยเขียวช้ำ มีโอกาสทำให้เนื้อตาย อาจทำให้อาเจียนเป็นเลือดหรือปัสสาวะเป็นเลือด",
-                "aid": "ขยับจุดที่โดนกัดให้น้อยที่สุด ควรทำความสะอาดแผลด้วยน้ำสะอาด หรือน้ำเกลือล้างแผล รีบไปพบแพทย์เพื่อทำการรักษาตั้งแต่เบื้องต้น ไม่ควรรักษาเอง **ห้ามใช้ปากดูดพิษเด็ดขาด**",
-                "type": "มีพิษ",
-                "colorStyle": "red"
+            # {
+            #     "name": "งูเขียวหางไหม้",
+            #     "accuracy": 2.0,
+            #     "symptoms": "ผิวหนังพองเป็นถุงน้ำ มีเลือดซึมออกจากแผลรอยเขี้ยวพบรอยเขียวช้ำ มีโอกาสทำให้เนื้อตาย อาจทำให้อาเจียนเป็นเลือดหรือปัสสาวะเป็นเลือด",
+            #     "aid": "ขยับจุดที่โดนกัดให้น้อยที่สุด ควรทำความสะอาดแผลด้วยน้ำสะอาด หรือน้ำเกลือล้างแผล รีบไปพบแพทย์เพื่อทำการรักษาตั้งแต่เบื้องต้น ไม่ควรรักษาเอง **ห้ามใช้ปากดูดพิษเด็ดขาด**",
+            #     "type": "มีพิษ",
+            #     "colorStyle": "red"
 
-            },
-            {
-                "name": "งูจงอาง",
-                "accuracy": 3.0,
-                "symptoms": "แสบบริเวณบาดแผล ปวดเล็กน้อย ทำให้กล้ามเนื้ออ่อนแรง หนังตาตก ทำให้มักเข้าใจผิดว่าผู้ป่วยง่วงนอน ต่อมาอาจมีกลืนลำบาก และเกิดอัมพาต",
-                "aid": "ควรรีบมาโรงพยาบาลโดยเร็วที่สุด และขยับส่วนที่ถูกกัดให้น้อยเฉพาะเท่าที่จำเป็น เพื่อลดการดูดซึมพิษงู ไม่แนะนำให้ขันชะเนาะ ทำความสะอาดบาดแผลเพื่อลดการติดเชื้อ",
-                "type": "มีพิษ",
-                "colorStyle": "red"
+            # },
+            # {
+            #     "name": "งูจงอาง",
+            #     "accuracy": 3.0,
+            #     "symptoms": "แสบบริเวณบาดแผล ปวดเล็กน้อย ทำให้กล้ามเนื้ออ่อนแรง หนังตาตก ทำให้มักเข้าใจผิดว่าผู้ป่วยง่วงนอน ต่อมาอาจมีกลืนลำบาก และเกิดอัมพาต",
+            #     "aid": "ควรรีบมาโรงพยาบาลโดยเร็วที่สุด และขยับส่วนที่ถูกกัดให้น้อยเฉพาะเท่าที่จำเป็น เพื่อลดการดูดซึมพิษงู ไม่แนะนำให้ขันชะเนาะ ทำความสะอาดบาดแผลเพื่อลดการติดเชื้อ",
+            #     "type": "มีพิษ",
+            #     "colorStyle": "red"
 
-            },
-            {
-                "name": "งูแมวเซา",
-                "accuracy": 4.0,
-                "symptoms": "เกิดอาการปวดและมีอาการบวมมาก เลือดออกตามไรฟันหรือไอมีเสมหะปนเลือด ถ่ายอุจาระสีดำปัสสาวะเป็นเลือด มีเลือดออกและการเแข็งตัวของเลือดผิดปกติ อาจทำให้ไตวายเฉียบพลัน",
-                "aid": "ควรรีบมาโรงพยาบาลโดยเร็วที่สุด และขยับส่วนที่ถูกกัดให้น้อยเฉพาะเท่าที่จำเป็น เพื่อลดการดูดซึมพิษงู **ห้ามใช้ปากดูดพิษเด็ดขาด**",
-                "type": "มีพิษ",
-                "colorStyle": "red"
+            # },
+            # {
+            #     "name": "งูแมวเซา",
+            #     "accuracy": 4.0,
+            #     "symptoms": "เกิดอาการปวดและมีอาการบวมมาก เลือดออกตามไรฟันหรือไอมีเสมหะปนเลือด ถ่ายอุจาระสีดำปัสสาวะเป็นเลือด มีเลือดออกและการเแข็งตัวของเลือดผิดปกติ อาจทำให้ไตวายเฉียบพลัน",
+            #     "aid": "ควรรีบมาโรงพยาบาลโดยเร็วที่สุด และขยับส่วนที่ถูกกัดให้น้อยเฉพาะเท่าที่จำเป็น เพื่อลดการดูดซึมพิษงู **ห้ามใช้ปากดูดพิษเด็ดขาด**",
+            #     "type": "มีพิษ",
+            #     "colorStyle": "red"
 
-            },
-            {
-                "name": "งูกะปะ",
-                "accuracy": 5.0,
-                "symptoms": "จะพบตุ่มน้ำเลือดหลายอัน และบางอันมีขนาดใหญ่ และมีเลือดออกจากแผลที่ถูกกัด เลือดออกตามไรฟันหรือไอมีเสมหะปนเลือด ถ่ายอุจาระสีดำปัสสาวะเป็นเลือด มีเลือดออกและการเแข็งตัวของเลือดผิดปกติ",
-                "aid": "ควรรีบมาโรงพยาบาลโดยเร็วที่สุด และขยับส่วนที่ถูกกัดให้น้อยเฉพาะเท่าที่จำเป็น เพื่อลดการดูดซึมพิษงู **ห้ามใช้ปากดูดพิษเด็ดขาด**",
-                "type": "มีพิษ",
-                "colorStyle": "red"
+  
+            #     "colorStyle": "red"
 
-            },
-            {
-                "name": "งูทับสมิงคลา",
-                "accuracy": 6.0,
-                "symptoms": "มีอาการหนักที่หนังตาบน ตาพร่า ต่อมาจะพบหนังตาตก ลืมตาไม่ขึ้น ตากลอกไปมาไม่ได้",
-                "aid": "ควรรีบมาโรงพยาบาลโดยเร็วที่สุด และขยับส่วนที่ถูกกัดให้น้อยเฉพาะเท่าที่จำเป็น เพื่อลดการดูดซึมพิษงู **ห้ามใช้ปากดูดพิษเด็ดขาด**",
-                "type": "มีพิษ",
-                "colorStyle": "red"
-            },
-            {
-                "name": "งูสามเหลี่ยม",
-                "accuracy": 7.0,
-                "symptoms": "มีอาการหนักที่หนังตาบน ตาพร่า ต่อมาจะพบหนังตาตก ลืมตาไม่ขึ้น ตากลอกไปมาไม่ได้",
-                "aid": "ควรรีบมาโรงพยาบาลโดยเร็วที่สุด และขยับส่วนที่ถูกกัดให้น้อยเฉพาะเท่าที่จำเป็น เพื่อลดการดูดซึมพิษงู **ห้ามใช้ปากดูดพิษเด็ดขาด**",
-                "type": "มีพิษ",
-                "colorStyle": "red"
-            }
+            # },
+            # {
+            #     "name": "งูทับสมิงคลา",
+            #     "accuracy": 6.0,
+            #     "symptoms": "มีอาการหนักที่หนังตาบน ตาพร่า ต่อมาจะพบหนังตาตก ลืมตาไม่ขึ้น ตากลอกไปมาไม่ได้",
+            #     "aid": "ควรรีบมาโรงพยาบาลโดยเร็วที่สุด และขยับส่วนที่ถูกกัดให้น้อยเฉพาะเท่าที่จำเป็น เพื่อลดการดูดซึมพิษงู **ห้ามใช้ปากดูดพิษเด็ดขาด**",
+            #     "type": "มีพิษ",
+            #     "colorStyle": "red"
+            # },
+            # {
+            #     "name": "งูสามเหลี่ยม",
+            #     "accuracy": 7.0,
+            #     "symptoms": "มีอาการหนักที่หนังตาบน ตาพร่า ต่อมาจะพบหนังตาตก ลืมตาไม่ขึ้น ตากลอกไปมาไม่ได้",
+            #     "aid": "ควรรีบมาโรงพยาบาลโดยเร็วที่สุด และขยับส่วนที่ถูกกัดให้น้อยเฉพาะเท่าที่จำเป็น เพื่อลดการดูดซึมพิษงู **ห้ามใช้ปากดูดพิษเด็ดขาด**",
+            #     "type": "มีพิษ",
+            #     "colorStyle": "red"
+            # }
         ],
             "EN": [
                 {
@@ -169,58 +163,58 @@ def upload():
                     "colorStyle": "red"
 
                 },
-                {
-                    "name": "GreenPitViper",
-                    "accuracy": 2.0,
-                    "symptoms": "Bruising&nbsp;of&nbsp;the&nbsp;skin blood&nbsp;in&nbsp;your&nbsp;urine Swelling&nbsp;in&nbsp;lymph&nbsp;nodes&nbsp;near&nbsp;the&nbsp;bite",
-                    "aid": "Should clean the wound with clean water Or saline Hurry to see a doctor for treatment from the beginning. Should not cure by yourself ** Do not use the mouth to suck venom at all **",
-                    "type": "Venomous",
-                    "colorStyle": "red"
+                # {
+                #     "name": "GreenPitViper",
+                #     "accuracy": 2.0,
+                #     "symptoms": "Bruising&nbsp;of&nbsp;the&nbsp;skin blood&nbsp;in&nbsp;your&nbsp;urine Swelling&nbsp;in&nbsp;lymph&nbsp;nodes&nbsp;near&nbsp;the&nbsp;bite",
+                #     "aid": "Should clean the wound with clean water Or saline Hurry to see a doctor for treatment from the beginning. Should not cure by yourself ** Do not use the mouth to suck venom at all **",
+                #     "type": "Venomous",
+                #     "colorStyle": "red"
 
-                },
-                {
-                    "name": "KingCobra",
-                    "accuracy": 3.0,
-                    "symptoms": "Drowsiness Hyporeflexia Ophthalmoplegia",
-                    "aid": "call emergency medical personnel to have them pick up and transport to hospital avoid sucking wound with mouth keep the limb as immobile as possible clean the wound with clean water Or saline not alcohol",
-                    "type": "Venomous",
-                    "colorStyle": "red"
+                # },
+                # {
+                #     "name": "KingCobra",
+                #     "accuracy": 3.0,
+                #     "symptoms": "Drowsiness Hyporeflexia Ophthalmoplegia",
+                #     "aid": "call emergency medical personnel to have them pick up and transport to hospital avoid sucking wound with mouth keep the limb as immobile as possible clean the wound with clean water Or saline not alcohol",
+                #     "type": "Venomous",
+                #     "colorStyle": "red"
 
-                },
-                {
-                    "name": "Russel viper",
-                    "accuracy": 4.0,
-                    "symptoms": "blood&nbsp;in&nbsp;your&nbsp;urine Local&nbsp;pain&nbsp;at&nbsp;bite&nbsp;site Vomiting Hypotension Bleeding&nbsp;gums acute&nbsp;kidney&nbsp;failure",
-                    "aid": "Should hurry to the hospital as soon as possible And move the bitten part to as little as necessary To reduce the absorption of snake venom ** Do not use the mouth to suck venom at all **",
-                    "type": "Venomous",
-                    "colorStyle": "red"
+                # },
+                # {
+                #     "name": "Russel viper",
+                #     "accuracy": 4.0,
+                #     "symptoms": "blood&nbsp;in&nbsp;your&nbsp;urine Local&nbsp;pain&nbsp;at&nbsp;bite&nbsp;site Vomiting Hypotension Bleeding&nbsp;gums acute&nbsp;kidney&nbsp;failure",
+                #     "aid": "Should hurry to the hospital as soon as possible And move the bitten part to as little as necessary To reduce the absorption of snake venom ** Do not use the mouth to suck venom at all **",
+                #     "type": "Venomous",
+                #     "colorStyle": "red"
 
-                },
-                {
-                    "name": "Malayan viper",
-                    "accuracy": 5.0,
-                    "symptoms": "local&nbsp;pain bruising inflammation blood&nbsp;in&nbsp;your&nbsp;urine Bleeding&nbsp;gums",
-                    "aid": "Should hurry to the hospital as soon as possible And move the bitten part to as little as necessary To reduce the absorption of snake venom ** Do not use the mouth to suck venom at all **",
-                    "type": "Venomous",
-                    "colorStyle": "red"
+                # },
+                # {
+                #     "name": "Malayan viper",
+                #     "accuracy": 5.0,
+                #     "symptoms": "local&nbsp;pain bruising inflammation blood&nbsp;in&nbsp;your&nbsp;urine Bleeding&nbsp;gums",
+                #     "aid": "Should hurry to the hospital as soon as possible And move the bitten part to as little as necessary To reduce the absorption of snake venom ** Do not use the mouth to suck venom at all **",
+                #     "type": "Venomous",
+                #     "colorStyle": "red"
 
-                },
-                {
-                    "name": "Malayan Krait",
-                    "accuracy": 6.0,
-                    "symptoms": "Heavy&nbsp;on&nbsp;the&nbsp;upper&nbsp;eyelid blurred&nbsp;vision the&nbsp;eyelids&nbsp;could&nbsp;not&nbsp;open.",
-                    "aid": "Should hurry to the hospital as soon as possible And move the bitten part to as little as necessary To reduce the absorption of snake venom   ** Do not use the mouth to suck venom at all **",
-                    "type": "Venomous",
-                    "colorStyle": "red"
-                },
-                {
-                    "name": "Banded Krait",
-                    "accuracy": 7.0,
-                    "symptoms": "Heavy&nbsp;on&nbsp;the&nbsp;upper&nbsp;eyelid blurred&nbsp;vision the&nbsp;eyelids&nbsp;could&nbsp;not&nbsp;open.",
-                    "aid": "Should hurry to the hospital as soon as possible And move the bitten part to as little as necessary To reduce the absorption of snake venom   **Do not use the mouth to suck venom at all **",
-                    "type": "Venomous",
-                    "colorStyle": "red"
-                }
+                # },
+                # {
+                #     "name": "Malayan Krait",
+                #     "accuracy": 6.0,
+                #     "symptoms": "Heavy&nbsp;on&nbsp;the&nbsp;upper&nbsp;eyelid blurred&nbsp;vision the&nbsp;eyelids&nbsp;could&nbsp;not&nbsp;open.",
+                #     "aid": "Should hurry to the hospital as soon as possible And move the bitten part to as little as necessary To reduce the absorption of snake venom   ** Do not use the mouth to suck venom at all **",
+                #     "type": "Venomous",
+                #     "colorStyle": "red"
+                # },
+                # {
+                #     "name": "Banded Krait",
+                #     "accuracy": 7.0,
+                #     "symptoms": "Heavy&nbsp;on&nbsp;the&nbsp;upper&nbsp;eyelid blurred&nbsp;vision the&nbsp;eyelids&nbsp;could&nbsp;not&nbsp;open.",
+                #     "aid": "Should hurry to the hospital as soon as possible And move the bitten part to as little as necessary To reduce the absorption of snake venom   **Do not use the mouth to suck venom at all **",
+                #     "type": "Venomous",
+                #     "colorStyle": "red"
+                # }
             ]
         }
         count = 0
